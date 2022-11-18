79906b24f06466fc7e41b55db08b77f70294523c
RezaRisaldi <reza.risaldi.ft21@mail.umy.ac.id>
2022-11-18 17:15:07 +0700

    if the node is to be inserted at the end of the list

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace double_linked_list
 {
@@ -52,6 +53,33 @@ namespace double_linked_list
                 START = newNode;
                 return;
             }
+            /*if the node is to be inserted at the end of the list*/
+            Node previous, current;
+            for(current = previous = START;
+                current != null && nim >= current.noMhs;
+                previous = current, current = current.next)
+            {
+                if (nim == current.noMhs)
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                }
+            }
+            /*On The execution of the above for loop, prev and
+             * current will point to those nodes
+             * between which the new node is to be inserted*/
+            newNode.next = current;
+            newNode.prev = previous;
+
+            //if the node is to be inserted at the end of the list
+            if (current == null)
+            {
+                newNode.next = null;
+                previous.next = newNode;
+                return;
+            }
+            current.prev = newNode;
+            previous.next = newNode;
         }
     }
     internal class Program
