e31ec69783898f6fcc7b512f873803f28343df16
RezaRisaldi <reza.risaldi.ft21@mail.umy.ac.id>
2022-11-18 17:00:33 +0700

    check if the list empty

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -36,6 +36,22 @@ namespace double_linked_list
             Node newNode = new Node();
             newNode.noMhs = nim;
             newNode.name = nm;
+
+            //check if the list empty
+            if (START == null || nim <= START.noMhs)
+            {
+                if ((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nDuplicate number not allowed");
+                    return;
+                }
+                newNode.next = START;
+                if (START !=null)
+                    START.prev = newNode;
+                newNode.next = null;
+                START = newNode;
+                return;
+            }
         }
     }
     internal class Program
